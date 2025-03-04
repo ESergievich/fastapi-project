9d002546fc6cbe2317c5972e8c5d1812e4e76d93
esergievich <sergievichevgeniy@gmail.com>
2025-03-04 03:22:16 +0300

    feat: include API router with main app

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ import uvicorn
 from fastapi import FastAPI
 
 from app.core import db_helper, settings
+from app.api import router as api_router
 
 
 @asynccontextmanager
@@ -12,7 +13,10 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     yield
     await db_helper.dispose()
 
+
 app = FastAPI(lifespan=lifespan)
+
+app.include_router(router=api_router)
 
 if __name__ == "__main__":
     uvicorn.run(
