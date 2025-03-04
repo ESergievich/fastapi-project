1ea2ba6ee50a3027b84ae941623f815d216d093f
esergievich <sergievichevgeniy@gmail.com>
2025-03-04 03:37:09 +0300

    feat: create users API routes with FastAPI-Users

---
+++ app/api/api_v1/users.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+
+from authentication import fastapi_users
+from core import settings
+from schemas import UserRead, UserUpdate
+
+router = APIRouter(
+    prefix=settings.api.v1.users,
+    tags=["Users"],
+)
+
+# /me
+# /{id}
+router.include_router(
+    router=fastapi_users.get_users_router(
+        UserRead,
+        UserUpdate,
+    ),
+)
