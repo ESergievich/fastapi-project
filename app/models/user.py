489a156c21d7f69a378a28167704ff79aa0e9df1
esergievich <sergievichevgeniy@gmail.com>
2025-03-11 22:35:34 +0300

    feat: add username, role fields for User

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,13 @@
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from sqlalchemy import String, Enum
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
+from utils import RoleEnum
 
 
 class User(Base, SQLAlchemyBaseUserTable[int]):
-    pass
+    username: Mapped[str] = mapped_column(String, nullable=False)
+    role: Mapped[RoleEnum] = mapped_column(
+        Enum(RoleEnum), nullable=False, server_default=RoleEnum.CUSTOMER.name
+    )
