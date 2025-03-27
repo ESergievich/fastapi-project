709607e5cbc4fb8fcd15a26ab47187604310bfad
esergievich <sergievichevgeniy@gmail.com>
2025-03-27 23:40:09 +0300

    feat: include cart_router with main app

--- app/api/api_v1/__init__.py
+++ app/api/api_v1/__init__.py
@@ -6,6 +6,7 @@ from .users import router as users_router
 from .base_router import create_base_router
 from .products import router as products_router
 from .orders import router as orders_router
+from .cart import router as cart_router
 
 router = APIRouter(
     prefix=settings.api.v1.prefix,
@@ -15,3 +16,4 @@ router.include_router(auth_router)
 router.include_router(users_router)
 router.include_router(products_router)
 router.include_router(orders_router)
+router.include_router(cart_router)
