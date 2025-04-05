3a28e7d205569062a19e792e068f34e7017f7e0c
esergievich <sergievichevgeniy@gmail.com>
2025-04-05 18:44:42 +0300

    feat: add http_bearer dependency for tokens

--- app/api/api_v1/users.py
+++ app/api/api_v1/users.py
@@ -1,12 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from fastapi.security import HTTPBearer
 
 from authentication import fastapi_users
 from core import settings
 from schemas import UserRead, UserUpdate
 
+http_bearer = HTTPBearer(auto_error=False)
+
 router = APIRouter(
     prefix=settings.api.v1.users,
     tags=["Users"],
+    dependencies=[Depends(http_bearer)],
 )
 
 # /me
