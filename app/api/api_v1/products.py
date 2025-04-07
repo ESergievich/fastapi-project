358be1ad8b0af3ad678f17db6360b223a51ceeeb
esergievich <sergievichevgeniy@gmail.com>
2025-04-07 03:04:01 +0300

    feat: add dependencies to products

--- app/api/api_v1/products.py
+++ app/api/api_v1/products.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from api.api_v1 import create_base_router
 from core import settings
+from dependencies import role_required
 from schemas import ProductUpdate, ProductResponse, ProductCreate, ProductFilterIn
 from service import product_service
+from utils import RoleEnum
 
 router = APIRouter(
     prefix=settings.api.v1.products,
@@ -17,5 +19,10 @@ router.include_router(
         update_schema=ProductUpdate,
         response_schema=ProductResponse,
         filter_in_schema=ProductFilterIn,
+        permission_map={
+            "create": role_required(RoleEnum.ADMIN, RoleEnum.MANAGER),
+            "update": role_required(RoleEnum.ADMIN, RoleEnum.MANAGER),
+            "delete": role_required(RoleEnum.ADMIN, RoleEnum.MANAGER),
+        },
     )
 )
