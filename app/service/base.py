814c198f8272979fb1114152d1cee81782f9a392
esergievich <sergievichevgeniy@gmail.com>
2025-04-08 05:04:42 +0300

    refactor: replaced basic exceptions to custom exceptions for base_service

--- app/service/base.py
+++ app/service/base.py
@@ -1,11 +1,9 @@
 from typing import Type, Generic, TYPE_CHECKING
 
-from fastapi import HTTPException
-from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
-from starlette import status
 
 from core import ModelType, UpdateSchemaType, CreateSchemaType
+from errors import ObjectNotFound, ForbiddenAccess
 from utils import RoleEnum
 
 if TYPE_CHECKING:
@@ -25,21 +23,15 @@ class BaseService(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         if "user_id" in object_in_data:
             self._check_user_permission(object_in_data["user_id"], current_user)
 
-        try:
-            object_db = await self.crud.create(object_in_data, session)
-        except IntegrityError as e:
-            error_detail = e.args[0].split("DETAIL:  ")[1]
-            raise HTTPException(status_code=409, detail=error_detail)
-        return object_db
+        return await self.crud.create(object_in_data, session)
 
     async def get_by_id(
         self, object_id: int, session: AsyncSession, current_user: "User"
     ) -> ModelType:
         object_db = await self.crud.get_by_id(object_id, session)
         if not object_db:
-            raise HTTPException(
-                status_code=404,
-                detail=f"{self.model.__name__} with ID {object_id} not found",
+            raise ObjectNotFound(
+                debug=f"{self.model.__name__} with ID {object_id} not found",
             )
         if current_user:
             self._check_ownership(object_db, current_user)
@@ -70,39 +62,28 @@ class BaseService(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         current_user: "User",
     ) -> ModelType:
         await self.get_by_id(object_id, session, current_user)
-
-        try:
-            update_data = object_in.model_dump(exclude_unset=True, exclude_none=True)
-            object_updated = await self.crud.update(object_id, update_data, session)
-        except IntegrityError as e:
-            error_detail = e.args[0].split("DETAIL:  ")[1]
-            raise HTTPException(status_code=409, detail=error_detail)
-        return object_updated
+        update_data = object_in.model_dump(exclude_unset=True, exclude_none=True)
+        return await self.crud.update(object_id, update_data, session)
 
     async def delete(
         self, object_id: int, session: AsyncSession, current_user: "User"
     ) -> None:
         object_db = await self.get_by_id(object_id, session, current_user)
-
-        try:
-            await self.crud.delete(object_db, session)
-        except IntegrityError as e:
-            error_detail = e.args[0].split("DETAIL:  ")[1]
-            raise HTTPException(status_code=409, detail=error_detail)
+        await self.crud.delete(object_db, session)
 
     @staticmethod
     def _check_user_permission(user_id: int, current_user: "User"):
         if current_user.role == RoleEnum.CUSTOMER and user_id != current_user.id:
-            raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN,
-                detail="You can't access object of another user",
+            raise ForbiddenAccess(
+                message="You can't access object of another user",
+                debug=f"user_id={user_id}, current={current_user.id}",
             )
 
     @staticmethod
     def _check_ownership(obj: ModelType, current_user: "User"):
         if current_user.role == RoleEnum.CUSTOMER and hasattr(obj, "user_id"):
             if obj.user_id != current_user.id:
-                raise HTTPException(
-                    status_code=status.HTTP_403_FORBIDDEN,
-                    detail="You don't own this object",
+                raise ForbiddenAccess(
+                    message="You don't own this object",
+                    debug=f"owner={obj.user_id}, current={current_user.id}",
                 )
